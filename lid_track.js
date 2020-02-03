d938d509a912ee865479e39b45bc65c1ebaae0b0
analytics-scripts <54020175+analytics-scripts@users.noreply.github.com>
2020-02-03 17:21:52 +0300

    Update lid_track.js

--- lid_track.js
+++ lid_track.js
@@ -1,43 +1,25 @@
-(function() {
-    if (Math.random() * 100 > 1) {
-        return null;
-    }
+(async () => {
+    if (document.cookie.includes("ulp_token")) {
+        const rawResponse = await fetch('https://www.aeroflot.ru/personal/services/internal/v.0.0.1/json/get_member_info', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'cookie': document.cookie
+            },
+            body: '{}'
+        });
 
-    try {
-        var constant_mock = window.fetch;
-        var headers_to_get_id = null;
+        const content = await rawResponse.json();
 
-        window.fetch = function() {
-            var args = arguments;
-
-            try {
-                if (args[0].includes("gw.aeroflot.io/api/pr/LKAB/UserLoyaltyProfile/v1/get")) {
-                    headers_to_get_id = args[1].headers;
-
-                    (async () => {
-                        const rawResponse = await constant_mock.apply(this, args);
-                        const content = await rawResponse.json();
-                    
-                        if (content.loyaltyInfo) {
-                            dataLayerSU.push({
-                                "event": "AeroinformEvent_78_2",
-                                "eventCategory": "loyaltyId",
-                                "eventAction": content.loyaltyInfo.loyaltyId,
-                                "eventLabel": null,
-                                "dimension5": "B"
-                            });
-                        }
-
-                        return rawResponse;
-                    })(); 
-                }
-            } catch (err) {
-                console.warn(err);
-            }
-        
-            return constant_mock.apply(this, args);
+        if (content.loyaltyInfo) {
+            dataLayerSU.push({
+                "event": "AeroinformEvent_78_2",
+                "eventCategory": "loyaltyId",
+                "eventAction": content.loyaltyInfo.loyaltyId,
+                "eventLabel": null,
+                "dimension5": "B"
+            });
         }
-    } catch (err) {
-        console.warn(err);
     }
 })();
